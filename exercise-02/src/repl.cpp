c07646b623184b13670bea62b4bfe914e3e26ec9
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2023-05-21 11:18:36 +0200

    Improve repl to handle multiple vars

--- exercise-02/src/repl.cpp
+++ exercise-02/src/repl.cpp
@@ -7,11 +7,49 @@
 
 using namespace std;
 
+typedef struct var {
+    string name;
+    float value;
+    struct var *next;
+} Variable;
+
+bool var_exists = false;
+bool var_is_writeable = false;
+
+// returns true if the variable was added successfully, false otherwise
+bool add_new_variable(string new_var_name, float new_var_value, Variable *&head) {
+    Variable *new_var = new Variable();
+    new_var->name = new_var_name;
+    new_var->value = new_var_value;
+    new_var->next = nullptr;
+
+    // list is empty, create new head
+    if (head == nullptr) {
+        head = new_var;
+        cout << "Created: " << new_var_name << " = " << new_var_value << endl;
+        return true;
+    }
+
+    // list is not empty, check if variable already exists
+    for (Variable *tmp = head; tmp != nullptr; tmp = tmp->next) {
+        if (tmp->name == new_var_name) {
+            tmp->value = new_var_value;
+            cout << "Updated: " << new_var_name << " = " << new_var_value << endl;
+            return true;
+        }
+    }
+
+    // variable does not exist, add it to the list
+    new_var->next = head;
+    head = new_var;
+    cout << "Created: " << new_var_name << " = " << new_var_value << endl;
+    return true;
+}
+
+// repl loop for evaluating infix expressions
 int main() {
-    string var;
-    float var_value;
-    bool var_exists = false;
-    bool var_is_writeable = false;
+    string new_var_name;
+    Variable *head = nullptr;
 
     while (true) {
         string inp;
@@ -20,6 +58,11 @@ int main() {
         getline(cin, inp);
 
         if (inp == "exit") {
+            for (Variable *tmp = head; tmp != nullptr; tmp = tmp->next) {
+                Variable *next = tmp->next;
+                delete tmp;
+                tmp = next;
+            }
             break;
 
         } else if (inp == "help") {
@@ -30,11 +73,12 @@ int main() {
             continue;
 
         } else if (inp.length() >= 4 && inp.substr(0, 4) == "set ") {
-            var = inp.substr(4, inp.length() - 4);
-            if (var.length() == 0 || var == " ") {
+            new_var_name = inp.substr(4, inp.length() - 4);
+            if (new_var_name.length() == 0 || new_var_name == " " || new_var_name == "set" || new_var_name == "help" || new_var_name == "exit") {
                 cout << "Invalid variable name." << endl;
                 continue;
             }
+
             var_exists = true;
             var_is_writeable = true;
             continue;
@@ -47,25 +91,33 @@ int main() {
 
             // variable has been set in previous prompt
             if (var_exists) {
-                // var has been created in previous prompt and should now be set to the result of the expression
+                // variable value should now be set to the result of the expression
                 if (var_is_writeable) {
-                    var_value = evaluate_infix(inp);
-                    cout << var << " = " << var_value << endl;
+                    float new_var_value = evaluate_infix(inp);
+                    if (!add_new_variable(new_var_name, new_var_value, head)) {
+                        cout << "Variable could not be created." << endl;
+                        continue;
+                    }
 
-                // replace all occurences of var with its value
+                // replace all occurences of variables with their value
                 } else {
-                    for (int i = 0; i < inp.length(); i++) {
-                        if (inp.find(var) == string::npos) {
-                            break;
+                    for (Variable *tmp = head; tmp != nullptr; tmp = tmp->next) {
+                        string var = tmp->name;
+                        string var_value = to_string(tmp->value);
+
+                        for (int i = 0; i < inp.length(); i++) {
+                            if (inp.find(var) == string::npos) {
+                                break;
+                            }
+                            inp = inp.replace(inp.find(var), var.length(), var_value);
                         }
-                        inp = inp.replace(inp.find(var), var.length(), to_string(var_value));
                     }
                     float result = evaluate_infix(inp);
                     cout << "Ergebnis: " << result << endl;
                 }
                 var_is_writeable = false;
 
-            // no variable set previously, just evaluate the expression
+            // no variables set previously, just evaluate the expression
             } else {
                 float result = evaluate_infix(inp);
                 cout << "Ergebnis: " << result << endl;
